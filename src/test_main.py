311a8461cedcfa95821ebc2db1bd6cd0c5dce3ca
Henry Liang <henryliang29@gmail.com>
2026-05-27 22:47:31 -0400

    updating imports

--- src/test_main.py
+++ src/test_main.py
@@ -9,11 +9,11 @@ from datetime import date, datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from src.MA_method import moving_average
+from MA_method import moving_average
 from massive import RESTClient
-from src.check_one_ticker import check_one_ticker
-from src.check_all_tickers import check_all_tickers
-from src.ticker_symbols import tickers
+from check_one_ticker import check_one_ticker
+from check_all_tickers import check_all_tickers
+from ticker_symbols import tickers
 
 
 
